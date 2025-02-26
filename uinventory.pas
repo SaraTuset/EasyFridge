f6a09054b9fb53ae5abddd3c54ab3abb69dcc900
SaraTuset <117302443+SaraTuset@users.noreply.github.com>
2025-02-26 23:48:37 +0100

    uProducto implementado del todo

--- uinventory.pas
+++ uinventory.pas
@@ -3,7 +3,7 @@ unit uInventory;
 
 interface
 
-uses uPila, uProducto, DateUtils;
+uses uPila, uProducto, SysUtils;
 
 type
   tInventory = tPila;
@@ -18,28 +18,30 @@ implementation
 procedure inicializarInventario(var p: tPila);
 begin
   initialize(p);  //Ponemos a nil
-  push(p, setProducto('Leche', Now));        //Vamos añadiendo los diferentes productos a la nevera
-  push(p, setProducto('Huevos', Now));
-  push(p, setProducto('Mantequilla', Now));
-  push(p, setProducto('Jamon', Now));
+  push(p, setProducto('Leche', EncodeDate(2024, 2, 20)));        //Vamos añadiendo los diferentes productos a la nevera
+  push(p, setProducto('Huevos', EncodeDate(2024, 2, 20)));
+  push(p, setProducto('Mantequilla', EncodeDate(2024, 2, 20)));
+  push(p, setProducto('Jamon', EncodeDate(2024, 2, 20)));
 end;
 
 PROCEDURE mostrarInventario(p: tPila);   //No hacemos var porque sino perderíamos la pila
 begin
   while not isEmpty(p) do  //Comprobamos que no esté vacía hasta que el puntero apunte a NIL
   begin
-    writeln(peek(p));  // Muestra el elemento en la cima
+    writeln(getNombre(peek(p)));  // Muestra el elemento en la cima
     p := p^.ant;        // Avanza al siguiente nodo sin modificar la pila original
   end;
 end;
 
 procedure annadirProducto(var p: tPila);
 var
-  nombre: String;
+  nombre, fecha: String;
 begin
   Write('Producto a añadir: ');
   Readln(nombre);
-  push(p, nombre);
+  Write('Fecha de caducidad (dd/mm/yyyy): ');
+  Readln(fecha);
+  push(p, setProducto(nombre, StrToDate(fecha)));
 end;
 
 procedure retirarDeInventario(var p: tPila; nombre: String);
@@ -49,7 +51,7 @@ begin
    initialize(aux);
    while not isEmpty(p) do
          begin
-           if peek(p) <> nombre then
+           if getNombre(peek(p)) <> nombre then
               begin
                 push(aux, peek(p));
               end;
